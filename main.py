261f218e33b368fb6d1fec5eb7172d8f1683cf19
CripyMeow <crispycat19@hotmail.com>
2021-11-08 04:57:07 +0700

    add wall_westcor and add 3room in westcor

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ width = 1280
 height = 720
 win = pygame.display.set_mode((width, height))
 pygame.display.set_caption("W <it> CH AcademY")
-bg = pygame.image.load("sprite/meetingroom.jpg")
+bg = pygame.image.load("sprite/entryhall.jpg")
 bg_width, bg_height = bg.get_rect().size
 #icon = pygame.image.load("sprite/icongame.png")
 #pygame.display.set_icon(icon)
@@ -22,7 +22,7 @@ start_scrolling_y = height/2
 stage_height = 720
 stage_position_y = 0
 
-X, Y, vel, WALKCOUNT, CHECK = 448, 238, 15, 0, 'DOWN'
+X, Y, vel, WALKCOUNT, CHECK = 508, 178, 15, 0, 'DOWN'
 
 mainClock = pygame.time.Clock()
 
@@ -32,7 +32,7 @@ DOWN, UP = False, False
 
 gohallway = False
 gopath = False
-gomeeting = True
+gomeeting = False
 gofirstaid = False
 gobattle = False
 gocanteen = False
@@ -41,7 +41,7 @@ goclass_2 = False
 goclass_3 = False
 gowestgar = False
 goeastgar = False
-goentry = False
+goentry = True
 gohalls = False
 
 gowestcor_1 = False
@@ -275,7 +275,7 @@ while run:
             run = False
         if keys[pygame.K_ESCAPE]:
             run = False
-#------------------------------------
+#--------------hallway---------------
     if gohallway == True: #HALLWAY
         # redrawsup()
         hall_way = [(0,223,0,103),(283,1280,0,103),(493,553,0,223),(538,1280,0,133),\
@@ -299,7 +299,7 @@ while run:
         elif Y >= 598:
             bg = pygame.image.load("sprite/entryhall.jpg")
             X = 508
-            Y = 47
+            Y = 43
             gohallway = False
             goentry = True
         elif Y <= 13:
@@ -326,7 +326,7 @@ while run:
         else:
             win.blit(bg ,(rel_x-bg_width, rel_y-bg_height))
             bg.blit(mapping[0], (0, 0))
-#------------------------------------
+#--------------canteen---------------
     elif gocanteen == True: #CANTEEN
         wall()
         rel_x = -X % bg_width
@@ -362,7 +362,7 @@ while run:
         else:
             stage_height = 950
             win.blit(bg ,(rel_x-bg_width, rel_y-bg_height))
-#------------------------------------
+#--------------eastcor1--------------
     elif goeastcor_1 == True:
         wall()
         rel_x = -X % bg_width
@@ -389,7 +389,7 @@ while run:
             win.blit(bg ,(-28, -178))
         else:
             win.blit(bg ,(-28, rel_y-bg_height))
-#------------------------------------
+#--------------eastcor2--------------
     elif goeastcor_2 == True:
         wall()
         rel_x = -X % bg_width
@@ -414,7 +414,7 @@ while run:
             goeastfor = True
         else:
             win.blit(bg ,(-28, -13))
-#------------------------------------
+#--------------eastgarden------------
     elif goeastgar == True:
         wall()
         rel_x = -X % bg_width
@@ -435,7 +435,7 @@ while run:
             goeastcor_1 = True
         else:
             win.blit(bg ,(rel_x-bg_width, rel_y-bg_height))
-#------------------------------------
+#--------------firstaid--------------
     elif gofirstaid == True: #FIRSTAID ROOM
         wallfirstaid = [(0,1280,0,178), (0,43,0,720), (0,238,583,720), (298,1280,583,720), (1153,1280,0,418), (1153,1280,523,720), (268,343,0,433), (568,643,0,433), \
         (0,118,343,433), (178,418,343,433,), (478,598,343,433), (28,88,0,298), (88,148,0,298), (148,208,0,298), (208,268,0,298), (328,388,0,298), (388,448,0,298), (448,508,0,298), (508,568,0,298), \
@@ -466,7 +466,7 @@ while run:
             win.blit(bg ,(-703, rel_y-bg_height))
         else:
             win.blit(bg ,(rel_x-bg_width, rel_y-bg_height))
-#------------------------------------
+#---------------battle---------------
     elif gobattle == True: #BATTLE ROOM
         wallbattle = [(0,43,0,313), (0,43,463,720), (0,1280,0,88), (0,1280,568,720), (1153,1280,0,253), (1153,1280,403,720), (43,1108,103,553)]
         wall(wallbattle)
@@ -493,7 +493,7 @@ while run:
             win.blit(bg ,(-523, -103))
         else:
             win.blit(bg ,(rel_x-bg_width, -103))
-#------------------------------------
+#----------------path----------------
     elif gopath == True:
         wall_path = [(0, 718, 0, 238), (748, 1280, 0, 238), (118, 658, 222, 297), (808, 1108, 222, 297), (0, 1280, 388, 1280)]
         wall(wall_path)
@@ -524,7 +524,7 @@ while run:
             win.blit(bg ,(-1108, -313))
         elif Y < 313 or Y >= 313:
             win.blit(bg ,(rel_x-bg_width, -313))
-#------------------------------------
+#--------------meeting---------------
     elif gomeeting == True: #MEETING ROOM
         meeting_wall = [(0,1280,0,103),(913,1280,0,133),(1033,1280,0,163),(1153,1280,0,720),(448,1280,568,720),(0,418,568,720),(0,43,0,720),(0,148,0,88),(0,163,0,133),(0,103,0,163),(298,388,103,148),(298,388,133,193)
                         ,(388,478,103,148),(388,493,133,198),(478,598,103,148),(478,613,133,198),(73,1063,193,253),(73,133,193,268),(973,1063,193,268),(1138,1280,193,268),(508,898,253,298),(508,898,283,328),(508,898,313,358)
@@ -547,10 +547,10 @@ while run:
             win.blit(bg ,(rel_x-bg_width, -418))
         else:
             win.blit(bg ,(rel_x-bg_width, rel_y-bg_height))
-#------------------------------------
+#--------------entryhall-------------
     elif goentry == True:
-        wall_entry = [(538,1280,0,92),(1213,1280,0,720),(538,1280,512,720),(0,493,512,720),(478,538,617,720),(0,43,242,720),(0,493,0,92),(583,1183,152,497),(53,433,152,497)\
-            ,(538,733,0,122),(928,1280,0,122),(298,493,0,122),(0,178,0,122),(178,298,0,107),(718,898,0,107)]
+        wall_entry =  [(538,1280,0,88),(1213,1280,0,720),(538,1280,512,720),(0,493,512,720),(478,538,617,720),(0,43,242,720),(0,493,0,88),(583,1183,152,497),(53,433,152,497)\
+            ,(538,733,0,118),(928,1280,0,118),(298,493,0,118),(0,178,0,118),(178,298,0,118),(718,898,0,118), (0,1280,598,720)]
         wall(wall_entry)
         rel_x = -X % bg_width
         rel_y = -Y % bg_height
@@ -574,7 +574,7 @@ while run:
             win.blit(bg ,(rel_x-bg_width, -272))
         else:
             win.blit(bg ,(rel_x-bg_width, rel_y-bg_height))
-#------------------------------------
+#----------------hall----------------
     elif gohalls == True:
         wall()
         rel_x = -X % bg_width
@@ -598,7 +598,7 @@ while run:
             win.blit(bg ,(rel_x-bg_width, -287))
         else:
             win.blit(bg ,(rel_x-bg_width, rel_y-bg_height))
-#------------------------------------
+#-------------westgarden-------------
     elif gowestgar == True:
         wall()
         rel_x = -X % bg_width
@@ -617,9 +617,11 @@ while run:
             gowestcor_1 = True
         else:
             win.blit(bg ,(rel_x-bg_width, rel_y-bg_height))
-#------------------------------------
+#--------------westcor1--------------
     elif gowestcor_1 == True:
-        wall()
+        wall_westcor_1 = [(0,688,388,720),(808,1280,388,720),(1168,1280,13,313),(1048,1280,88,313),(1048,1280,0,73),(328,853,0,43),(0,163,0,103),(88,163,0,133),(88,163,133,343),(0,163,163,313),(88,163,343,720),(328,868,43,133),(629,928,118,283)
+                ,(628,928,283,343),(268,568,283,343),(268,343,0,343),(853,928,0,343),(268,568,118,283)]
+        wall(wall_westcor_1)
         rel_x = -X % bg_width
         rel_y = -Y % bg_height
 
@@ -644,15 +646,35 @@ while run:
             win.blit(bg ,(-28, -365))
         else:
             win.blit(bg ,(-28, rel_y-bg_height))
-#------------------------------------
+        if 13 <= X <= 28 and 313 <= Y < 388:
+            win.fill((255,0,0), rect=[X+20,Y-50,50,50])
+            if keys[pygame.K_f]:
+                bg = pygame.image.load("sprite/apothecaryroom.jpg")
+                X = 973
+                Y = 433
+                gowestcor_1 = False
+                goapothe = True
+                CHECK = "LEFT"
+        if 13 <= X <= 28 and 103 <= Y < 163:
+            win.fill((255,0,0), rect=[X+20,Y-50,50,50])
+            if keys[pygame.K_f]:
+                bg = pygame.image.load("sprite/teacherroom.jpg")
+                X = 958
+                Y = 373
+                gowestcor_1 = False
+                goteach = True
+                CHECK = "LEFT"
+#--------------westcor2--------------
     elif gowestcor_2 == True:
-        wall()
+        wall_westcor_2 = [(328,868,553,720),(328,868,373,523),(268,343,313,720),(858,928,313,720),(268,928,313,403),(688,1280,0,103),(988,1280,0,223),(1048,1280,0,598),(1168,1280,0,720),(688,1280,0,163),(748,868,0,223),(868,998,0,223)
+                ,(0,508,0,163),(0,208,0,223),(208,328,0,223),(328,448,0,223),(88,163,0,283),(88,163,313,720),(0,163,358,720)]
+        wall(wall_westcor_2)
         rel_x = -X % bg_width
         rel_y = -Y % bg_height
 
         if Y >= 613:
             bg = pygame.image.load("sprite/westcorridor_1.jpg")
-            Y = 73
+            Y = 103
             gowestcor_2 = False
             gowestcor_1 = True
         elif Y <= 13:
@@ -661,9 +683,20 @@ while run:
             Y = 598
             gowestcor_2 = False
             gowestfor = True
+        elif Y >= 73:
+            win.blit(bg ,(-28, -73))
         else:
-            win.blit(bg ,(-28, -13))
-#------------------------------------
+            win.blit(bg ,(-28, rel_y-bg_height))
+        if 13 <= X <= 28 and 208 <= Y < 358:
+            win.fill((255,0,0), rect=[X+20,Y-50,50,50])
+            if keys[pygame.K_f]:
+                bg = pygame.image.load("sprite/researchroom.jpg")
+                X = 973
+                Y = 433
+                gowestcor_2 = False
+                goresearch = True
+                CHECK = "LEFT"
+#-------------westforest-------------
     elif gowestfor == True:
         wall()
         rel_x = -X % bg_width
@@ -684,7 +717,7 @@ while run:
             win.blit(bg ,(-583, -58))
         else:
             win.blit(bg ,(rel_x-bg_width, -58))
-#------------------------------------
+#--------------forest----------------
     elif goforest == True:
         wall()
         rel_x = -X % bg_width
@@ -708,7 +741,7 @@ while run:
             win.blit(bg ,(rel_x-bg_width, -425))
         else:
             win.blit(bg ,(rel_x-bg_width, rel_y-bg_height))
-#------------------------------------
+#-------------eastforest--------------
     elif goeastfor == True:
         wall()
         rel_x = -X % bg_width
@@ -729,6 +762,63 @@ while run:
             win.blit(bg ,(-583, -58))
         else:
             win.blit(bg ,(rel_x-bg_width, -58))
+#------------apothecaryroom-----------
+    elif goapothe == True:
+        wall()
+        rel_x = -X % bg_width
+        rel_y = -Y % bg_height
+
+        if X >= 400:
+            win.blit(bg ,(-400, -58))
+        else:
+            win.blit(bg ,(rel_x-bg_width, -58))
+        if 1033 <= X <= 1630 and 358 <= Y < 493:
+            win.fill((255,0,0), rect=[X+20,Y-50,50,50])
+            if keys[pygame.K_f]:
+                bg = pygame.image.load("sprite/westcorridor_1.jpg")
+                X = 58
+                Y = 343
+                gowestcor_1 = True
+                goapothe = False
+                CHECK = "RIGHT"
+#------------teacherroom--------------
+    elif goteach == True:
+        wall()
+        rel_x = -X % bg_width
+        rel_y = -Y % bg_height
+
+        if X >= 400:
+            win.blit(bg ,(-400, -58))
+        else:
+            win.blit(bg ,(rel_x-bg_width, -58))
+        if 1033 <= X <= 1630 and 358 <= Y < 493:
+            win.fill((255,0,0), rect=[X+20,Y-50,50,50])
+            if keys[pygame.K_f]:
+                bg = pygame.image.load("sprite/westcorridor_1.jpg")
+                X = 58
+                Y = 133
+                gowestcor_1 = True
+                goteach = False
+                CHECK = "RIGHT"
+#------------researchroom--------------
+    elif goresearch == True:
+        wall()
+        rel_x = -X % bg_width
+        rel_y = -Y % bg_height
+
+        if X >= 400:
+            win.blit(bg ,(-400, -58))
+        else:
+            win.blit(bg ,(rel_x-bg_width, -58))
+        if 1033 <= X <= 1630 and 358 <= Y < 493:
+            win.fill((255,0,0), rect=[X+20,Y-50,50,50])
+            if keys[pygame.K_f]:
+                bg = pygame.image.load("sprite/westcorridor_2.jpg")
+                X = 58
+                Y = 298
+                gowestcor_2 = True
+                goteach = False
+                CHECK = "RIGHT"
 #---------------------------------------------------------------------------
     
     # print(X, Y)
